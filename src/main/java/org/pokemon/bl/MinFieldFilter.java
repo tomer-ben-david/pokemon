8a827fc8f8922915a09e4fa9c563c0448949d726
tomer-ben-david <tomerbd1@gmail.com>
2023-09-08 09:56:51 +0300

    test

--- src/main/java/org/pokemon/bl/MinFieldFilter.java
+++ src/main/java/org/pokemon/bl/MinFieldFilter.java
@@ -7,8 +7,8 @@ import java.util.Comparator;
 
 @AllArgsConstructor
 public class MinFieldFilter implements FilterTwo {
-    private final Comparator<Pokemon> maxComparator;
+    private final Comparator<Pokemon> minComparator;
     public Pokemon filter(Pokemon pokemon1, Pokemon pokemon2) {
-        return maxComparator.compare(pokemon1, pokemon2) < 0 ? pokemon1 : pokemon2;
+        return minComparator.compare(pokemon1, pokemon2) < 0 ? pokemon1 : pokemon2;
     }
 }
